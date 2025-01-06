24d8308defe3d4208607d90c803636b954c599fa
Mayanksankhla2003 <sankhlamayank2003@gmail.com>
2025-01-06 23:30:22 +0530

    user authentication and authorization

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const {
+    registerUser,
+    loginUser,
+    logout,
+} = require("../controllers/authController");
 
 router.get("/", (req, res) => {
     res.send("working users router");
 });
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/logout", logout);
 
 module.exports = router;
